0fb96c2b10bdd04332e03ee99eef0c2bb91cfa5b
Nash_Oppong <nashoppong95@gmail.com>
2025-01-09 01:37:39 +0000

    adding in app navigations

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
-import Splash from "./src/screens/auth/splash/splash";
+
 import AHeader from "./src/components/AuthHeader/AHeader";
 import SignUpPage from "./src/screens/auth/signup/SignUpPage";
 import LoginPage from "./src/screens/auth/login/LoginPage";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Splash from "./src/screens/auth/splash/Splash";
+import colors from "./src/constants/colors";
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const theme = {
+    colors: { background: colors.white },
+  };
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <Splash />
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator theme={theme} options>
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="signup" component={SignUpPage} />
+        <Stack.Screen name="LoginIn" component={LoginPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
